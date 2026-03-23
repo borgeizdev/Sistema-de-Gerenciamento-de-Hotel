a0d9ba95aff49a39253265fc12d456d63ac7a874
borgeiz <guilhermecordeiroc25@gmail.com>
2026-03-23 19:05:41 -0300

    Primeiro Commit!

---
+++ src/main/java/com/mycompany/projetindadri/ProjetindaDri.java
@@ -0,0 +1,14 @@
+package com.mycompany.projetindadri;
+
+public class ProjetindaDri {
+
+    public static void main(String[] args) {
+         String[] hospede = new String[100];
+        int[] reserva = new int[100];
+        int[] quarto = new int[100];
+        boolean[] statusquarto = new boolean[100];
+
+        int[][] consumofrigobar = new int [100][4];
+
+    }
+}
